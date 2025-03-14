7cfff51923b77cc3274ce0f0a9215ca01f35b22e
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-14 19:31:31 +0300

    RegistrationForm
    on line 10 RegistrationForm JFrame  was created.

--- registrationform/src/main/java/com/example/registrationform/RegistrationForm.java
+++ registrationform/src/main/java/com/example/registrationform/RegistrationForm.java
@@ -1,4 +1,16 @@
 package com.example.registrationform;
 
-public class RegistrationForm {
+import javax.swing.*;
+import java.awt.*;
+
+public class RegistrationForm extends JFrame {
+    private JTextField emailField, nameField, courseField;
+    private JTextArea hobbiesArea;
+
+    public RegistrationForm() {
+        setTitle("Registration Form");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(5, 2));
+    }
 }
